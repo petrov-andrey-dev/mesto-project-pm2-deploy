a47160019ec856a0cfc9cc528270c9d91d44d135
petrov-andrey-dev <petrov.a.v-14@yandex.ru>
2024-06-28 23:55:16 +0300

    add ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+require('dotenv').config({ path: './.env.deploy' });
+
+const { HOST, USER, REPO, PATH, REF = 'origin/master' } = process.env;
+
+module.exports = {
+  apps: [
+    {
+      name: 'mesto-backend',
+      script: './backend/src/app.ts',
+      env: {
+        NODE_ENV: 'production',
+      },
+    }
+  ],
+  deploy: {
+    production: {
+      user: USER,
+      host: HOST,
+      ref: REF,
+      repo: REPO,
+      path: PATH,
+      'pre-deploy-local': `scp ./backend/.env ${USER}@${HOST}:${PATH}/backend/.env`,
+      'post-deploy': `cd ./backend && npm i && pm2 reload ecosystem.config.js --env production `,
+    },
+  },
+};
